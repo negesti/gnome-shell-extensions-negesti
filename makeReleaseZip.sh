31ab7a7b9a6e5984a30d3c2e9ee8ca96d603ec28
negesti <clemens@lab21.org>
2017-06-13 17:16:32 +0200

    fix makeReleaseZip.sh

--- makeReleaseZip.sh
+++ makeReleaseZip.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 TARGET="putWindow@clemens.lab21.org.zip"
 
-cd putWindow@clemens.lab21.org
-
 echo "recompiling schema file..."
 cd schemas
 make
 cd ..
 
 echo "Zip it"
-zip -r $TARGET *
-
-mv $TARGET ..
+zip -r $TARGET *.js metadata.json schemas/gschemas.compiled locale messages.pot 
 
 # http://whatthecommit.com/0e0c1a4060a298158f3c4ef526f03f86
 echo "
@@ -21,5 +17,6 @@ echo "
             Bunny approves these changes
 
     http://whatthecommit.com/c3fa809433d7131c1bb92e4ba0d372b8
+
 "
 
